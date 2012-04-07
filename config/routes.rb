5f5620f2c29129e9907aa64d525cd3db75cf2562
Steven! Ragnarök <steven@nuclearsandwich.com>
2012-04-07 13:08:46 -0700

    Add search controller and view.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Heorot::Application.routes.draw do
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
 
+  match "/search" => "search#show"
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
@@ -48,7 +47,7 @@ Heorot::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => "search#show"
 
   # See how all your routes lay out with "rake routes"
 
---
+++ features/step_definitions/search_steps.rb
@@ -0,0 +1,4 @@
+When /^I search for the book's title$/ do
+  visit search_path title: @book.title
+end
+
