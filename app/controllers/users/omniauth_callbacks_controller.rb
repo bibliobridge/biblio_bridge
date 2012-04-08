59ddbb024f381505b4de38fd6d75e3dfa731f887
Steven! Ragnarök <steven@nuclearsandwich.com>
2012-04-08 09:11:07 -0700

    Add what I *think* will be enough for FB login.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,7 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
-    @user = User.find_or_create_from_facebook
+    user = User.find_or_create_from_facebook request.env["omniauth.auth"]
+    sign_in_and_redirect user, event: :authentication
   end
-
 end
+
