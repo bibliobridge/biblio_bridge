938be9493419ae63cc7f203d950da1bec390694d
Cifani <mcifani@gmail.com>
2012-04-08 17:26:01 -0700

    fixing flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
-module ApplicationHelper
+module ApplicationHelper\
+
+	def flash_message(type, text)
+	    flash[type] ||= []
+	    flash[type] << text
+	end
+
+	def render_flash
+	  rendered = []
+	  flash.each do |type, messages|
+	    messages.each do |m|
+	      rendered << render(:partial => 'partials/flash', :locals => {:type => type, :message => m}) unless m.blank?
+	    end
+	  end
+	  rendered.join('<br/>')
+	end
+
 end
